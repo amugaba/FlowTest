ab9f08c44b8c03af73e704507f9dc9ea43575008
David Tidd <tiddd@indiana.edu>
2014-01-24 12:10:45 -0500

    Implemented feature 002 - HelloGit

---
+++ FlowTest/src/HelloGit.java
@@ -0,0 +1,13 @@
+
+public class HelloGit {
+
+	public HelloGit() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello Git!");
+
+	}
+
+}
